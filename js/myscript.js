3ea91eeda423228487027bfd1aabb51d8d653a67
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-05-02 21:49:33 +0200

    [UPD] mail complete

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const emails=['rispolifrancesco@gmail.com','stanleykubrick@gmail.com','yoshihirotogashi@gmail.com'];
+const button=document.querySelector('#button-addon2');
+
+button.addEventListener('click', function(){
+    const email=document.querySelector('input').value;
+    for(let i=0; i < email.length; i++){
+        if(email===emails[i]){
+            alert('sei nella lista');
+            console.log(email);
+            retrun;
+        }
+    }
+    alert('non sei nella lista');
+})
